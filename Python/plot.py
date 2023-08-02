b0498f871b6c0452d35322d15c9166f8c336b46b
gamer301 <priyanshu123sah@gmail.com>
2023-08-02 15:05:38 +0530

    printf

--- Python/plot.py
+++ Python/plot.py
@@ -4,5 +4,8 @@ import numpy as np
 xpoints = np.array([1, 8])
 ypoints = np.array([3, 10])
 
-plt.plot(xpoints, ypoints,'*')
+plt.scatter(xpoints, ypoints,marker='+',ms-25,mc='b',label='random') 
+# u can just use x or y point and it wil take default value from 1,2,3,4 with step of 1 
+#plt.plot(xpoints,marker='H') if u dont use market argument the point won't be connected and only specific points will be marked withou joining them with line 
+
 plt.show()
