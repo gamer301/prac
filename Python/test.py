1ead18fce281be06eeb847987c08c6f127f4591c
gamer301 <priyanshu123sah@gmail.com>
2023-08-02 12:14:39 +0530

    python binary

--- Python/test.py
+++ Python/test.py
@@ -7,6 +7,12 @@
 #print ("Good afternoon" if time == "afternoon" else "good morning", end=" " )
 #print(f"{name}")
 
+def binary(num):
+    if num < 0 :
+      return "Erorr"
+    return bin(num)[2:] # we use [2:] to slice and start from 3rd element since python write 0b in front binary to diffrentiate 
+    # we cal also use bin().replace("0b", "")
+
 def greeting(name,time):
     if time =="morning":
         print ("Good Morning")
@@ -19,6 +25,10 @@ def greeting(name,time):
     return f"{name}"
 
 
-name = input("Enter name : ")
-time = input("Enter time:")
-print (greeting(name ,time)) 
+# name = input("Enter name : ")
+# time = input("Enter time:")
+# print (greeting(name ,time)) 
+
+num = binary(int(input("Enter num for conversion :")))
+print (num)
+
