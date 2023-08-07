37004e36779edcf2eebaac2142e704bd69301fca
gamer301 <priyanshu123sah@gmail.com>
2023-08-07 20:28:48 +0530

    evening sync

--- Java/Human.java
+++ Java/Human.java
@@ -4,14 +4,15 @@ public class Human {
 	int age;
 	double weight;
 	
-	Human(String name,int age,double weight)
+	Human(String name,int age,double weight) // constructor , same name as class , runs once when class is instancized with new.class_name
     {		
 		this.name = name;
 		this.age = age;
 		this.weight = weight;
 	}
 	
-	void eat() {
+	void eat() // method 
+    {
 		System.out.println(this.name+" is eating");
 	}
 	void drink() {
