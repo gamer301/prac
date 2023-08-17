c8b15b88c893b7786b2c1942ce8e4aa2b63abde1
gamer301 <priyanshu123sah@gmail.com>
2023-08-17 20:01:30 +0530

    Update class_work.py

--- Python/class_work.py
+++ Python/class_work.py
@@ -4,12 +4,19 @@ class vehicle :
     self.capacity=capacity
     self.color=color
     self.fare = fare + capacity*100
+  
+  """
+  def fare(self)
+    fare=self.capacity*100 
+    return fare
+  """   
 
+    
 veh1=vehicle()
 print(veh1.capacity,veh1.color,veh1.fare)
 
 class bus(vehicle):
-
+print(self.fare)
   
   def __init__(self, capacity=50, color="Red",fare=0):
     self.cap_bus=capacity
@@ -17,6 +24,10 @@ class bus(vehicle):
     self.fare= fare+capacity*100
     print(self.fare) 
     self.total_fare= self.fare + ((self.fare /100)*10)
+
+    """
+    self.total_fare= fare() + ((fare() /100)*10)
+    """
 
 bus1=bus()
 print(bus1.total_fare)
