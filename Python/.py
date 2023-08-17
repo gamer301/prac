6eb60c1945e6e38ac36102f9fa8c74b50a52ee67
gamer301 <priyanshu123sah@gmail.com>
2023-08-17 19:48:52 +0530

    Update .py

--- Python/.py
+++ Python/.py
@@ -1,17 +1,26 @@
-class vehicle 
+class vehicle :
 
-  def __init__(self,capacity=4,color="White")
+  def __init__(self,capacity=4,color="White",fare=0) :
     self.capacity=capacity
     self.color=color
+    self.fare = fare + capacity*100
 
-  def fare (capacity)
-    fare = capacity * 100 
+veh1=vehicle()
+print(veh1.capacity,veh1.color,veh1.fare)
 
- class bus(vehicle)
+class bus(vehicle):
 
-  def __init__(self, capacity=50, color="Red"):
-    super().__init__(capacity, color) 
-    
-       
+  
+  def __init__(self, capacity=50, color="Red",fare=0):
+    self.cap_bus=capacity
+    self.col_bus=capacity
+    self.fare= fare+capacity*100
+    print(self.fare) 
+    self.total_fare= self.fare + ((self.fare /100)*10)
+
+bus1=bus()
+print(bus1.total_fare)
+
+
 
     
