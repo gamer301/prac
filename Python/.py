fa9ee80ee11aad2aefd22dec4a000d5a72830111
gamer301 <priyanshu123sah@gmail.com>
2023-08-17 19:12:05 +0530

    Create .py

---
+++ Python/.py
@@ -0,0 +1,17 @@
+class vehicle 
+
+  def __init__(self,capacity=4,color="White")
+    self.capacity=capacity
+    self.color=color
+
+  def fare (capacity)
+    fare = capacity * 100 
+
+ class bus(vehicle)
+
+  def __init__(self, capacity=50, color="Red"):
+    super().__init__(capacity, color) 
+    
+       
+
+    
