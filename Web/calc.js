d78af111d7d5700f251fa1ea6d7960bc9261e8e7
gamer301 <priyanshu123sah@gmail.com>
2023-08-19 13:25:25 +0530

    web proj ip

---
+++ Web/calc.js
@@ -0,0 +1,90 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById("display");
+    const buttons = document.querySelectorAll("button");
+
+    let currentInput = "";
+    let operator = "";
+    let firstOperand = "";
+    let secondOperand = "";
+    let result = "";
+
+    buttons.forEach(button => 
+        {
+        button.addEventListener("click", function () 
+        {
+            const buttonText = button.textContent;
+
+            if (buttonText === "C") {
+                clear();
+            } else if (buttonText === "=") {
+                calculate();
+            } else if (buttonText === "&#9003;") {
+                backspace();
+            } else if (button.classList.contains("operator")) {
+                handleOperator(buttonText);
+            } else {
+                currentInput += buttonText;
+                updateDisplay();
+            }
+        });
+    });
+
+    function clear() {
+        currentInput = "";
+        operator = "";
+        firstOperand = "";
+        secondOperand = "";
+        result = "";
+        updateDisplay();
+    }
+
+    function calculate() {
+        if (firstOperand && operator && currentInput) {
+            secondOperand = currentInput;
+            switch (operator) {
+                case "+":
+                    result = parseFloat(firstOperand) + parseFloat(secondOperand);
+                    break;
+                case "-":
+                    result = parseFloat(firstOperand) - parseFloat(secondOperand);
+                    break;
+                case "*":
+                    result = parseFloat(firstOperand) * parseFloat(secondOperand);
+                    break;
+                case "/":
+                    result = parseFloat(firstOperand) / parseFloat(secondOperand);
+                    break;
+            }
+            currentInput = result.toString();
+            operator = "";
+            firstOperand = "";
+            secondOperand = "";
+            updateDisplay();
+        }
+    }
+
+    function backspace() 
+    {
+        currentInput = currentInput.slice(-1);
+        currentInput = currentInput.replace("&#9003;"," ")
+        updateDisplay();
+    }
+
+    function handleOperator(op) 
+    {        
+         if (currentInput) 
+          {
+            firstOperand = currentInput;
+            operator = op;
+            currentInput = currentInput + operator;
+            currentInput = currentInput.replace("&#9003;","")
+            updateDisplay();  
+          }   
+    }
+
+    function updateDisplay() 
+    {
+
+        display.value = currentInput || "0";
+    }
+});
