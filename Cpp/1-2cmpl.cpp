c094087e97fcca90b5f679e50c62f7e4b85dfe4b
gamer301 <priyanshu123sah@gmail.com>
2023-11-09 23:54:40 +0530

    shift to VSC 2022

---
+++ Cpp/1-2cmpl.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int n =0 ;
+    cout<<"Enter length on Bin";
+    cin>>n;
+    int carry=1;
+    char bin[n+1];
+    char cmp_2[n+1];
+    char cmp_2[n+1];
+    cout<<"Enter Bin";
+    cin>>bin;
+    cout<<bin;
+
+
+}
---
+++ Java/oral.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class oral 
+{
+    public static void main (String[] args) 
+    {
+        double x = Double.POSITIVE_INFINITY ;
+        int y=3;
+        int z = Integer.MAX_VALUE;
+        if (x>y)
+        {
+            System.out.println(x);
+            System.out.println(z);
+        }
+    }
+}
